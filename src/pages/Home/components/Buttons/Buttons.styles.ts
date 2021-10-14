5149a30da900896e04473b21bde2e9e4758934b0
Egor Stronhin <i@egor.xyz>
2021-10-14 23:36:07 +0300

    wip

--- src/pages/Home/components/Buttons/Buttons.styles.ts
+++ src/pages/Home/components/Buttons/Buttons.styles.ts
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   margin-top: 20px;
   font-size: 0;
-  padding: 0 40px;
+  padding: 0 60px 0 20px;
   > div {
     box-sizing: border-box;
     position: relative;
     display: inline-block;
     width: 50%;
-    height: 150px;
+    height: 130px;
     /* border: 1px solid #000; */
   }
   svg {
