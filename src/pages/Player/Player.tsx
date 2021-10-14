452519b585415ef3098bb7fc8579d73e7ee5b39e
Egor Stronhin <i@egor.xyz>
2021-10-14 21:31:24 +0300

    wip

--- src/pages/Player/Player.tsx
+++ src/pages/Player/Player.tsx
@@ -10,9 +10,9 @@ min-height: 100vh;
 margin: 0px;
 position: relative;
 background-color: #000;
-img {
-  width: 50%;
-  height: auto;
+.video {
+  height: 100%;
+  overflow: hidden;
 }
 .audio {
   display: none;
@@ -31,23 +31,25 @@ button {
 
 export const Player: FC = () => {
   return (<Wrapper>
-
-    <video
-      height="100%"
-      width="100%"
-      className="video-player__viewer"
-      video-player-target="video"
-      data-action="click->video-player--video-player#togglePlay timeupdate->video-player--video-player#progressUpdate loadedmetadata->video-player--video-player#setVideoDuration"
-      src="https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"
-      disablePictureInPicture
-      autoPlay
-      playsInline
-      controls={false}
-      loop
-      muted
-    />
+    <div className="video">
+      <video
+        // height="100%"
+        // width="100%"
+        className="video-player__viewer"
+        video-player-target="video"
+        data-action="click->video-player--video-player#togglePlay timeupdate->video-player--video-player#progressUpdate loadedmetadata->video-player--video-player#setVideoDuration"
+        src="https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"
+        disablePictureInPicture
+        autoPlay
+        playsInline
+        controls={false}
+        loop
+        muted
+      />
+    </div>
 
     <button onClick={() => document.getElementById('audio').play()}>Play</button>
+
     <div className="audio">
       <audio controls id="audio">
         <source
