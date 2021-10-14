5715395a21238a9477a74003e8bb179a83d9326e
Egor Stronhin <i@egor.xyz>
2021-10-14 21:28:12 +0300

    wip

--- src/pages/Player/Player.tsx
+++ src/pages/Player/Player.tsx
@@ -9,26 +9,19 @@ align-items: center;
 min-height: 100vh;
 margin: 0px;
 position: relative;
+background-color: #000;
 img {
   width: 50%;
   height: auto;
 }
-.container {
-  position: absolute;
-}
 .audio {
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  left: 50%;
-  bottom: 20px;
+  display: none;
   audio {
     transform: translateX(-50%);
-    display: none;
   }
 }
 button {
-  position: absolute;
+  position: fixed;
   left: 20px;
   bottom: 20px;
   font-size: 30px;
@@ -38,33 +31,31 @@ button {
 
 export const Player: FC = () => {
   return (<Wrapper>
-    <div className="container">
-      <div style={{ marginTop: '-161px' }}>
-        <video
-          height="100%"
-          width="100%"
-          className="video-player__viewer"
-          video-player-target="video"
-          data-action="click->video-player--video-player#togglePlay timeupdate->video-player--video-player#progressUpdate loadedmetadata->video-player--video-player#setVideoDuration"
-          src="https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"
-          disablePictureInPicture
-          autoPlay
-          playsInline
-          controls={false}
-          loop
-          muted
+
+    <video
+      height="100%"
+      width="100%"
+      className="video-player__viewer"
+      video-player-target="video"
+      data-action="click->video-player--video-player#togglePlay timeupdate->video-player--video-player#progressUpdate loadedmetadata->video-player--video-player#setVideoDuration"
+      src="https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"
+      disablePictureInPicture
+      autoPlay
+      playsInline
+      controls={false}
+      loop
+      muted
+    />
+
+    <button onClick={() => document.getElementById('audio').play()}>Play</button>
+    <div className="audio">
+      <audio controls id="audio">
+        <source
+          src="https://www.mindfulness-network.org/wp-content/uploads/2018/09/Breath-and-the-Body-6-mins-and-30-sec-9MB.mp3"
+          type="audio/mpeg"
         />
-      </div>
-      <button onClick={() => document.getElementById('audio').play()}>Play</button>
-      <div className="audio">
-        <audio controls id="audio">
-          <source
-            src="https://www.mindfulness-network.org/wp-content/uploads/2018/09/Breath-and-the-Body-6-mins-and-30-sec-9MB.mp3"
-            type="audio/mpeg"
-          />
-          Your browser does not support the audio element.
-        </audio>
-      </div>
+        Your browser does not support the audio element.
+      </audio>
     </div>
   </Wrapper >)
 }
