7f1a3c5ff78814e3aea90e559b8e392aabe41486
Egor Stronhin <i@egor.xyz>
2021-10-14 18:51:38 +0300

    wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,19 @@
 import { FC } from "react";
+import styled from "styled-components";
+import logo from 'assets/McHeart.png';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  img {
+    width: 40%;
+  }
+`;
 
 export const Home: FC = () => (
-  <div>Home</div>
+  <Wrapper>
+    <img src={logo} alt="logo" />
+  </Wrapper>
 );
