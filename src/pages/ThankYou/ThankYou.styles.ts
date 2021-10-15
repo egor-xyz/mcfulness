577e6ea4cb8d7c2a40e7b8c7a235d46a8b26db2c
Egor Stronhin <i@egor.xyz>
2021-10-15 09:17:53 +0300

    wip

--- src/pages/ThankYou/ThankYou.styles.ts
+++ src/pages/ThankYou/ThankYou.styles.ts
@@ -25,21 +25,28 @@ export const Title = styled.div`
   font-weight: bold;
 `;
 
-export const Text = styled.div`
+export const Footer = styled.div`
   position: fixed;
   left: 0;
-  bottom: 250px;
+  width: 100%;
+  bottom: 60px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Text = styled.div`
+  bottom: 270px;
   width: 100%;
   font-size: 40px;
   line-height: 70px;
   font-weight: 600;
   font-size: 48px;
   color: #fff;
+  margin-bottom: 40px;
 `;
 
 export const StyledFivetips = styled(Fivetips)`
-  position: absolute;
-  left: 50%;
-  bottom: 40px;
-  transform: translateX(-50%);
+  display: block;
+  margin: 0 auto;
 `;
--- src/pages/ThankYou/ThankYou.tsx
+++ src/pages/ThankYou/ThankYou.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 import { StyledLogo } from 'pages/Home/Home.styles';
 
-import { StyledFivetips, Wrapper, Title, Text, StyledYBG } from './ThankYou.styles';
+import { StyledFivetips, Wrapper, Title, Text, StyledYBG, Footer } from './ThankYou.styles';
 
 export const ThankYou: FC = () => (<Wrapper>
   <StyledYBG />
   <Link to='/'><StyledLogo /></Link>
   <Title>Your Order<br />is ready</Title>
-  <Text>Sit down,<br /> Slow down<br />and<br />Enjoy</Text>
-  <StyledFivetips />
+  <Footer >
+    <Text>Sit down,<br /> Slow down<br />and<br />Enjoy</Text>
+    <StyledFivetips />
+  </Footer>
 </Wrapper>);
