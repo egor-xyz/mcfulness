58944d76fce1b29ac5c2e53972518609fa85a3df
Egor Stronhin <i@egor.xyz>
2021-10-15 08:40:23 +0300

    wip

--- src/pages/ThankYou/ThankYou.styles.ts
+++ src/pages/ThankYou/ThankYou.styles.ts
@@ -1,21 +1,32 @@
 import styled from 'styled-components';
 
+import { YBG } from 'assets';
+
 export const Wrapper = styled.div`
-.bg {
-  position: fixed;
-  bottom: -30%;
+  max-width: 450px;
+  position: relative;
+  margin: 0 auto;
+  min-height: 100vh;
+`;
+
+export const StyledYBG = styled(YBG)`
+  position: absolute;
+  top: 20%;
   left: 0;
+  width: 100%;
   z-index: -1;
-}
 `;
 
 export const Text = styled.div`
-  margin-top: 140px;
+  margin-top: 40px;
   font-size: 40px;
 `;
 
 export const Text2 = styled.div`
-  margin-top: 120px;
+  position: absolute;
+  margin-top: 35%;
+  left: 0;
+  width: 100%;
   font-size: 35px;
   color: #fff;
 `;
--- src/pages/ThankYou/ThankYou.tsx
+++ src/pages/ThankYou/ThankYou.tsx
@@ -2,12 +2,11 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import { StyledLogo } from 'pages/Home/Home.styles';
-import { YBG } from 'assets';
 
-import { Wrapper, Text, Text2 } from './ThankYou.styles';
+import { Wrapper, Text, Text2, StyledYBG } from './ThankYou.styles';
 
 export const ThankYou: FC = () => (<Wrapper>
-  <YBG className='bg' />
+  <StyledYBG />
   <Link to='/'><StyledLogo /></Link>
   <Text>Your Order is ready</Text>
   <Text2>Sit down,<br /> Slow down<br />and<br />Enjoy</Text2>
